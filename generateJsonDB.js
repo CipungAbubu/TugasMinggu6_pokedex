2629d7688d82ae28845942cd5a50ddbaf0672f6f
CipungAbubu <maryaheba08@gmail.com>
2024-10-23 00:57:22 +0700

    menulis data ke dalam sebuah file bernama .db.json

--- generateJsonDB.js
+++ generateJsonDB.js
@@ -38,7 +38,7 @@ async function generateJsonDB() {
     payload.push(item);
     console.log(detail);
   }
-
+  fs.writeFileSync(".db.json", JSON.stringify(payload, null, 2), "utf8");
   console.log(payload);
   } catch (error) {
     console.error(error);
