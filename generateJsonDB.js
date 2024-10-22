b97acf145b70ad6ccfa9bd5daf81c9dbceba09e2
CipungAbubu <maryaheba08@gmail.com>
2024-10-23 01:42:06 +0700

    berhasil melakukan npm run start dan localhost:300 bisa di akses

--- generateJsonDB.js
+++ generateJsonDB.js
@@ -7,7 +7,7 @@ async function generateJsonDB() {
   // dan ketika akses url http://localhost:3000/pokemon akan muncul seluruh data
   // pokemon yang telah kalian parsing dari public api pokemon
 
-  const pokemonApiURL = "https://pokeapi.co/api/v2/pokemon/?limit=20";
+  const pokemonApiURL = "https://pokeapi.co/api/v2/pokemon/?limit=100";
   const pokemonList = await fetch(pokemonApiURL).then((res) => res.json());
   const payload = [];
   for (let index = 0; index < pokemonList.results.length; index++) {
